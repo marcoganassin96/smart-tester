6ff0a77649c35faca1834fa045720b6baddd730c
m.ganassin <m.ganassin@reply.it>
2023-10-18 19:15:09 +0200

    [FIX] Smart searcher - Manually fixed previous errors in generating unit tests

--- src/python/smart-tester/smarttester/tests/utils/test_text_utils.py
+++ src/python/smart-tester/smarttester/tests/utils/test_text_utils.py
@@ -5,19 +5,21 @@ from smarttester.utils.text_utils import _get_bullets_number
 # unit tests
 # below, each test case is represented by a tuple passed to the @pytest.mark.parametrize decorator
 
+# MANUALLY FIXED: Smart Tester was not able to understand that different bullet characters must be counted as
+# different bullets
 @pytest.mark.parametrize(
     "text, expected",
     [
         ("", 0),  # Empty string
         ("This is a regular text without bullets.", 0),  # Text without bullets
         ("• Bullet 1\n• Bullet 2\n• Bullet 3", 3),  # Single bullet format
-        ("• Bullet 1\n- Bullet 2\n* Bullet 3\n- Bullet 4", 4),  # Multiple bullet formats
-        ("• Bullet 1\n• Bullet 2\n• Bullet 3 with numbers 123", 0),  # Bullets with alphanumeric characters
-        ("• Bullet 1\n• Bullet 2\n• Bullet 3 with special characters @#$", 0),  # Bullets with special characters
-        ("This is a regular line.\n• Bullet 1\nThis is another regular line.\n- Bullet 2\nThis is a third regular line.", 2),  # Mixed bullet and non-bullet lines
-        ("• Bullet 1\n• Bullet 1\n• Bullet 2\n• Bullet 2\n• Bullet 2", 3),  # Duplicate bullets
-        ("• Bullet 1\n- Bullet 2\n* Bullet 3", 3),  # Different bullet characters
-        (" • Bullet 1\n• Bullet 2 \n • Bullet 3 ", 3),  # Leading and trailing spaces in bullet lines
+        ("• Bullet 1\n- Bullet 2\n* Bullet 3\n- Bullet 4", 2),  # Multiple bullet formats
+        ("• Bullet 1\n• Bullet 2\n• Bullet 3 with numbers 123", 3),  # Bullets with alphanumeric characters
+        ("• Bullet 1\n• Bullet 2\n• Bullet 3 with special characters @#$", 3),  # Bullets with special characters
+        ("This is a regular line.\n• Bullet 1\nThis is another regular line.\n- Bullet 2\nThis is a third regular line.", 1),  # Mixed bullet and non-bullet lines
+        ("• Bullet 1\n• Bullet 1\n• Bullet 2\n• Bullet 2\n• Bullet 2", 5),  # Duplicate bullets
+        ("• Bullet 1\n- Bullet 2\n* Bullet 3", 1),  # Different bullet characters
+        (" • Bullet 1\n• Bullet 2 \n • Bullet 3 ", 2),  # Leading and trailing spaces in bullet lines
         ("This is a regular text without bullets.\nThis is another line.\nAnd another line.", 0),
         # Text with multiple lines but no bullets
         ("• Bullet 1\n\n• Bullet 2\n\n\n• Bullet 3", 3),  # Text with empty lines
