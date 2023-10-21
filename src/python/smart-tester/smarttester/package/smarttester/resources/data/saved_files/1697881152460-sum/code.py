9c93bad0c545a5a735edd838e47eb76a99eb670e
m.ganassin <m.ganassin@reply.it>
2023-10-21 14:29:44 +0200

    [ADD] Smart Tester - Script to verify smart testing run from step 2 - Regenerated all intermediate output starting by plan

--- src/python/smart-tester/smarttester/package/smarttester/resources/data/saved_files/1697881152460-sum/code.py
+++ src/python/smart-tester/smarttester/package/smarttester/resources/data/saved_files/1697881152460-sum/code.py
@@ -6,36 +6,15 @@ def sum(a: int, b: int) -> int:
     return a + b
 
 # unit tests
-@pytest.mark.parametrize("a, b, expected", [
-    # Basic scenarios
-    (2, 3, 5),
-    (10, 20, 30),
-    (-5, -10, -15),
-    (-100, -200, -300),
-    (5, -10, -5),
-    (100, -200, -100),
-    (-5, 10, 5),
-    (-100, 200, 100),
-    (0, 0, 0),
-    (0, -5, -5),
-    # Edge cases
-    (1000000, 2000000, 3000000),
-    (-1000000, -2000000, -3000000),
-    (None, 5, pytest.raises(TypeError)),
-    (10, None, pytest.raises(TypeError)),
-    (None, None, pytest.raises(TypeError)),
-    (3.14, 5, pytest.raises(TypeError)),
-    (10, "20", pytest.raises(TypeError)),
-    ("hello", "world", pytest.raises(TypeError)),
-    # Special cases
-    (5, 5, 10),
-    (-10, -10, -20),
-    (1000000000000000000000000, 1, 1000000000000000000000001),
-    (-999999999999999999999999, 1000000000000000000000000, 1)
+@pytest.mark.parametrize("a, b, expected_result", [
+    (5, 10, 15),                     # Positive integers
+    (-5, -10, -15),                  # Negative integers
+    (0, 0, 0),                       # Zero
+    (1000000, 2000000, 3000000),     # Large integers
+    (0, 10, 10),                     # One input is zero
+    (-1000000, 500000, -500000),     # One input is a large negative number
+    (1000000, 500000, 1500000),      # One input is a large positive number
 ])
-def test_sum(a, b, expected):
-    if isinstance(expected, int):
-        assert sum(a, b) == expected
-    else:
-        with expected:
-            sum(a, b)
+def test_sum(a, b, expected_result):
+    # Assert that the actual result of the sum function matches the expected result
+    assert sum(a, b) == expected_result
